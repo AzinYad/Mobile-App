5ab340b992acd08eb37e3f64358ddd9e522265b1
AzinYad <azin.yadegari@gmail.com>
2023-06-01 18:14:07 +0100

    create li element

--- script.js
+++ script.js
@@ -24,10 +24,14 @@ addButtonEl.addEventListener("click", function () {
 });
 
 onValue(shoppingListInDB, function (snapshot) {
-    let itemsArray = Object.values(snapshot.val());
+    let itemsArray = Object.entries(snapshot.val());
     clearShoppingListEl();
     for (let i = 0; i < itemsArray.length; i++) {
-        appendItemToShoppingListEl(itemsArray[i]);
+        let currentItem = itemsArray[i];
+        let currentItemID = currentItem[0];
+        let currentItemValue = currentItem[1];
+
+        appendItemToShoppingListEl(currentItem);
     };
 });
 
@@ -41,7 +45,12 @@ const clearInputFieldEl = () => {
     inputFieldEl.value = "";
 };
 
-const appendItemToShoppingListEl = (itemValue) => {
-    shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+const appendItemToShoppingListEl = (item) => {
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li");
+    newEl.textContent = itemValue;
+    shoppingListEl.append(newEl);
 };
 // ========================
